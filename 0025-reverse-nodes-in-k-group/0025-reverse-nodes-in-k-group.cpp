4b7bb789f4e19b3381f271d28e298777acff6ad1
Sandeepjha2102 <144441543+Sandeepjha2102@users.noreply.github.com>
2025-09-15 21:22:55 +0530

    Time: 0 ms (100%), Space: 16.6 MB (38.72%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,48 +11,100 @@
  */
 class Solution {
 public:
-    // Helper function to reverse a linked list from head to tail
-    ListNode* reverse(ListNode* head, ListNode* tail) {
-        ListNode* prev = tail->next;
-        ListNode* curr = head;
-        while (prev != tail) {
-            ListNode* nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
+    // // Helper function to reverse a linked list from head to tail
+    // ListNode* reverse(ListNode* head, ListNode* tail) {
+    //     ListNode* prev = tail->next;
+    //     ListNode* curr = head;
+    //     while (prev != tail) {
+    //         ListNode* nxt = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = nxt;
+    //     }
+    //     return tail;
+    // }
+
+    // ListNode* reverseKGroup(ListNode* head, int k) {
+    //     if (!head || k == 1) return head;
+
+    //     ListNode dummy(0);
+    //     dummy.next = head;
+    //     ListNode* prevGroup = &dummy;
+
+    //     while (true) {
+    //         // find kth node
+    //         ListNode* kth = prevGroup;
+    //         for (int i = 0; i < k && kth; i++) {
+    //             kth = kth->next;
+    //         }
+    //         if (!kth) break; // less than k nodes remain
+
+    //         ListNode* groupStart = prevGroup->next;
+    //         ListNode* groupEnd = kth;
+    //         ListNode* nextGroup = kth->next;
+
+    //         // reverse the group
+    //         reverse(groupStart, groupEnd);
+
+    //         // reconnect links
+    //         prevGroup->next = groupEnd;
+    //         groupStart->next = nextGroup;
+
+    //         // move prevGroup
+    //         prevGroup = groupStart;
+    //     }
+    //     return dummy.next;
+
+
+    //REVERSE THE LINKED LIST
+
+    ListNode* reverseLinkedList(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+
+        while(temp != NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
         }
-        return tail;
+        return prev;
+    }
+
+    ListNode* getKthNode(ListNode* temp, int k){
+        k -=1;
+        while(temp != NULL && k>0){
+            k--;
+            temp = temp->next;
+        }
+        return temp;
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k == 1) return head;
+        ListNode* temp = head;
+        ListNode* prevlast = NULL;
 
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prevGroup = &dummy;
-
-        while (true) {
-            // find kth node
-            ListNode* kth = prevGroup;
-            for (int i = 0; i < k && kth; i++) {
-                kth = kth->next;
+        while(temp != NULL){
+            ListNode* kThNode = getKthNode(temp, k);
+            if(kThNode == NULL){
+                prevlast->next = temp;
+                break;
             }
-            if (!kth) break; // less than k nodes remain
 
-            ListNode* groupStart = prevGroup->next;
-            ListNode* groupEnd = kth;
-            ListNode* nextGroup = kth->next;
+            ListNode* nextNode = kThNode->next;
+            kThNode->next = NULL;
 
-            // reverse the group
-            reverse(groupStart, groupEnd);
+            reverseLinkedList(temp);
 
-            // reconnect links
-            prevGroup->next = groupEnd;
-            groupStart->next = nextGroup;
-
-            // move prevGroup
-            prevGroup = groupStart;
-        }
-        return dummy.next;
+            if(temp == head){
+                head = kThNode;
+            }
+            else{
+                prevlast->next = kThNode;
+            }
+            prevlast = temp;
+            temp = nextNode;
+            }
+            return head;
     }
 };
