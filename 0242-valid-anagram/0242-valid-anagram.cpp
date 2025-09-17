2dde803dd44c83680a63a01c805bd186cc90dc4a
Sandeepjha2102 <144441543+Sandeepjha2102@users.noreply.github.com>
2025-09-17 20:27:26 +0530

    Time: 5 ms (29.89%), Space: 10 MB (11.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,44 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        if(s.size() != t.size()){
-            return false;
-        }
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // if(s.size() != t.size()){
+        //     return false;
+        // }
 
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        // for(int i=0; i<s.size(); i++){
+        //     if(s[i] != t[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
+        if(s.size() != t.size()) return false;
+
+        //storing elements of s string into unordered map
+        unordered_map<char,int> mpp;
+
+        //iterate over string s
         for(int i=0; i<s.size(); i++){
-            if(s[i] != t[i]){
+            mpp[s[i]]++;
+        }
+
+        //compare with t string and decrement the values
+        for(int i=0; i<t.size(); i++){
+            if(mpp.find(t[i]) != mpp.end()){
+                mpp[t[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+
+        //iterate over map to check value of keys
+        for(auto it : mpp){
+            if(it.second != 0){
                 return false;
             }
         }
