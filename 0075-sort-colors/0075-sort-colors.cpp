f97fbc80490113ac2ab039dbb763afde9755e4b8
Sandeepjha2102 <144441543+Sandeepjha2102@users.noreply.github.com>
2026-01-07 02:05:37 +0530

    Time: 0 ms (100%), Space: 10.3 MB (99.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int countZero = 0;
-        int countOne = 0;
-        int countTwo = 0;
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 0) countZero++;
-            else if(nums[i] == 1) countOne++;
-            else countTwo++;
-        }
-
-        cout << countZero;
-        cout << countOne;
-        cout << countTwo;
-
-        for(int i=0; i<countZero; i++){
-            nums[i] = 0;
-        }
-        for(int i=countZero; i<(countZero+countOne); i++){
-            nums[i] = 1;
-        }
-        for(int i=(countZero+countOne); i<(countZero+countOne+countTwo); i++){
-            nums[i] = 2;
+        int l, m, r;
+        l = 0;
+        m = 0;
+        r = nums.size() - 1;
+        while (m <= r) {
+            if (nums[m] == 0) {
+                swap(nums[m], nums[l]);
+                l++;
+                m++;
+            }
+            else if (nums[m] == 1)
+                m++;
+            else {
+                swap(nums[m], nums[r]);
+                r--;
+            }
         }
     }
 };
