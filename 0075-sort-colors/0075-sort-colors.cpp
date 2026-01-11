63cd05cab109a931e52c43c6ca2ff14dec2cab3f
Sandeepjha2102 <144441543+Sandeepjha2102@users.noreply.github.com>
2026-01-11 23:15:41 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l, m, r;
-        l = 0;
-        m = 0;
-        r = nums.size() - 1;
-        while (m <= r) {
-            if (nums[m] == 0) {
-                swap(nums[m], nums[l]);
-                l++;
-                m++;
+        // int countZero = 0;
+        // int countOne = 0;
+        // int countTwo = 0;
+
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] == 0) countZero++;
+        //     else if(nums[i] == 1) countOne++;
+        //     else countTwo++;
+        // }
+
+        // cout << countZero;
+        // cout << countOne;
+        // cout << countTwo;
+
+        // for(int i=0; i<countZero; i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i=countZero; i<(countZero+countOne); i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i=(countZero+countOne); i<(countZero+countOne+countTwo); i++){
+        //     nums[i] = 2;
+        // }
+
+
+        //DUTCH NATIONAL FLAG
+        int low = 0;
+        int mid = 0;
+        int high = nums.size()-1;
+
+        while(mid <= high){
+            if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
             }
-            else if (nums[m] == 1)
-                m++;
-            else {
-                swap(nums[m], nums[r]);
-                r--;
+            else if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else{
+                //best case when mid == 1;
+                mid++;
             }
         }
     }
