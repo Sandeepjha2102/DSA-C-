7e11651b62d03fa8e2daed318939d61fa0755a57
Sandeepjha2102 <144441543+Sandeepjha2102@users.noreply.github.com>
2026-01-12 00:26:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.21%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findsq(int n){
+        int sum = 0;
+        while(n > 0){
+            int d = n % 10;
+            n = n / 10;
+            sum = sum + (d*d);
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(fast != 1){
+            slow = findsq(slow);
+            fast = findsq(fast);
+            fast = findsq(fast);
+
+            if(slow == fast && slow != 1){
+                return false;
+            }
+        }  
+        return true;  
+        }
+};
