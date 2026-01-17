6d03ba37cfcbe835fcb59cdf556e1aa9586ac337
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-01-17 19:11:54 +0530

    Time: 0 ms (100%), Space: 23.1 MB (5.55%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back({newInterval[0], newInterval[1]});
+        vector<vector<int>> res1;
+        // int start = intervals[0][0];
+        bool insert = false;
+        for(int i = 0; i<intervals.size(); i++){
+            if(insert == false && intervals[i][0] >= newInterval[0]){
+                res1.push_back({newInterval[0], newInterval[1]});
+                insert = true;
+            }
+            res1.push_back(intervals[i]);
+        }
+        if(insert == false){
+            res1.push_back({newInterval[0], newInterval[1]});
+        }
 
-        sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>> ans;
-        int n = intervals.size();
+        vector<vector<int>> res2;
+        int start1 = res1[0][0];
+        int end1 = res1[0][1];
 
-        int start1 = intervals[0][0];
-        int end1 = intervals[0][1];
-
-
-        for(int i = 1; i < n; i++){
-            int start2 = intervals[i][0];
-            int end2 = intervals[i][1];
+        for(int i = 1; i<res1.size(); i++){
+            int start2 = res1[i][0];
+            int end2 = res1[i][1];
 
             if(end1 >= start2){
                 end1 = max(end1, end2);
                 continue;
             }
             else{
-                ans.push_back({start1, end1});
+                res2.push_back({start1, end1});
                 start1 = start2;
                 end1 = end2;
             }
         }
-            ans.push_back({start1, end1});
-            return ans;
+        res2.push_back({start1, end1});
+        return res2;
     }
 };
