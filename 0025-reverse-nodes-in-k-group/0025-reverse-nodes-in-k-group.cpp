7de43a8018d88e35faff507d5f059960168eb153
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-01-31 03:20:17 +0530

    Time: 0 ms (100%), Space: 16.5 MB (37.88%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,7 +1,6 @@
 /**
  * Definition for singly-linked list.
- * struct Li
- stNode {
+ * struct ListNode {
  *     int val;
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
@@ -11,100 +10,31 @@
  */
 class Solution {
 public:
-    // // Helper function to reverse a linked list from head to tail
-    // ListNode* reverse(ListNode* head, ListNode* tail) {
-    //     ListNode* prev = tail->next;
-    //     ListNode* curr = head;
-    //     while (prev != tail) {
-    //         ListNode* nxt = curr->next;
-    //         curr->next = prev;
-    //         prev = curr;
-    //         curr = nxt;
-    //     }
-    //     return tail;
-    // }
-
-    // ListNode* reverseKGroup(ListNode* head, int k) {
-    //     if (!head || k == 1) return head;
-
-    //     ListNode dummy(0);
-    //     dummy.next = head;
-    //     ListNode* prevGroup = &dummy;
-
-    //     while (true) {
-    //         // find kth node
-    //         ListNode* kth = prevGroup;
-    //         for (int i = 0; i < k && kth; i++) {
-    //             kth = kth->next;
-    //         }
-    //         if (!kth) break; // less than k nodes remain
-
-    //         ListNode* groupStart = prevGroup->next;
-    //         ListNode* groupEnd = kth;
-    //         ListNode* nextGroup = kth->next;
-
-    //         // reverse the group
-    //         reverse(groupStart, groupEnd);
-
-    //         // reconnect links
-    //         prevGroup->next = groupEnd;
-    //         groupStart->next = nextGroup;
-
-    //         // move prevGroup
-    //         prevGroup = groupStart;
-    //     }
-    //     return dummy.next;
-
-
-    //REVERSE THE LINKED LIST
-
-    ListNode* reverseLinkedList(ListNode* head){
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-
-        while(temp != NULL){
-            ListNode* front = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = front;
-        }
-        return prev;
-    }
-
-    ListNode* getKthNode(ListNode* temp, int k){
-        k -=1;
-        while(temp != NULL && k>0){
-            k--;
-            temp = temp->next;
-        }
-        return temp;
-    }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp = head;
-        ListNode* prevlast = NULL;
+        int cnt = 0;
 
-        while(temp != NULL){
-            ListNode* kThNode = getKthNode(temp, k);
-            if(kThNode == NULL){
-                prevlast->next = temp;
-                break;
-            }
+        //Checking kth nodes are present or not
+        while(cnt < k){
+            if(temp == NULL) return head;
+            temp = temp->next;
+            cnt++;
+        }
 
-            ListNode* nextNode = kThNode->next;
-            kThNode->next = NULL;
+        //use recursive call to get next connecting node
+        ListNode* prevNode = reverseKGroup(temp, k);
 
-            reverseLinkedList(temp);
+        //connectinh curr nodes
+        temp = head;
+        cnt = 0;
 
-            if(temp == head){
-                head = kThNode;
-            }
-            else{
-                prevlast->next = kThNode;
-            }
-            prevlast = temp;
-            temp = nextNode;
-            }
-            return head;
+        while(cnt < k){
+            ListNode* next = temp->next;
+            temp->next = prevNode;
+            prevNode = temp;
+            temp = next;
+            cnt++;
+        }
+        return prevNode;
     }
 };
