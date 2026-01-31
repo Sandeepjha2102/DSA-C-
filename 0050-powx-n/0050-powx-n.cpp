48a89462721034cb02c974ccc4e3c98e64d74452
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-02-01 03:12:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,31 +1,7 @@
-// class Solution {
-// public:
-
-//     double solve(double x, long n){
-//         if(n==0){
-//             return 1;
-//         }
-//         if(n % 2 == 0) {
-//             return solve(x*x, n/2);
-//         }
-//         else{
-//             return x*solve(x*x, ((n-1)/2));
-//         }
-//     }
-
-//     double myPow(double x, int n) {
-//         long nn = n;
-//         if(n<0) return 1/solve(x,-nn);
-//         return solve(x,nn);
-//     }
-// };
-
-
 class Solution {
 public:
-
     double myPow(double x, int n) {
-        long long m = abs((long long)n); // prevent overflow
+         long long m = abs((long long)n); // prevent overflow
        double ans = 1;
        while(m > 0){
         if(m % 2 == 1){
