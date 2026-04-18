214dad7b7e4f3bf2d046ee2e8378dd5e7c4a8fdb
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-18 20:41:57 +0530

    Time: 0 ms (100%), Space: 21.9 MB (20.43%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res1;
-        // int start = intervals[0][0];
-        bool insert = false;
-        for(int i = 0; i<intervals.size(); i++){
-            if(insert == false && intervals[i][0] >= newInterval[0]){
-                res1.push_back({newInterval[0], newInterval[1]});
-                insert = true;
-            }
-            res1.push_back(intervals[i]);
+        vector<vector<int>> res;
+
+        int i = 0;
+        int n = intervals.size();
+        //left part
+        while(i < n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
         }
-        if(insert == false){
-            res1.push_back({newInterval[0], newInterval[1]});
+        //middle part
+        while(i < n && intervals[i][0] <= newInterval[1]){
+           newInterval[0] = min(intervals[i][0], newInterval[0]);
+           newInterval[1] = max(intervals[i][1], newInterval[1]); 
+           i++;
+        }
+        res.push_back({newInterval[0], newInterval[1]});
+
+        //right part
+        while(i < n){
+            res.push_back(intervals[i]);
+            i++;
         }
 
-
-        vector<vector<int>> res2;
-        int start1 = res1[0][0];
-        int end1 = res1[0][1];
-
-        for(int i = 1; i<res1.size(); i++){
-            int start2 = res1[i][0];
-            int end2 = res1[i][1];
-
-            if(end1 >= start2){
-                end1 = max(end1, end2);
-                continue;
-            }
-            else{
-                res2.push_back({start1, end1});
-                start1 = start2;
-                end1 = end2;
-            }
-        }
-        res2.push_back({start1, end1});
-        return res2;
+        return res;
     }
 };
