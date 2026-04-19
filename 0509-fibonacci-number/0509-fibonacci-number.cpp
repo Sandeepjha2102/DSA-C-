ee488c70f7d7484b97cdfe821bbbd5354acfea1f
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-19 12:53:33 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.85%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int f(int n, vector<int> &dp){
-        if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n];
-        int last = f(n-1, dp);
-        int slast = f(n-2, dp);
-        return last+slast;
-    }
-
     int fib(int n) {
-        vector<int> dp(n+1, -1);
-        return f(n, dp);
+
+        if(n == 1 || n == 0) return n;
+
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
