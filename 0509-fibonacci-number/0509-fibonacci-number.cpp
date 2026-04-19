a9cb564084349cc7e3882c42561c0ab5ab584d3f
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-19 14:07:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -5,13 +5,14 @@ public:
         if(n == 1 || n == 0) return n;
 
         vector<int> dp(n+1);
-        dp[0] = 0;
-        dp[1] = 1;
-
+            int prev = 1;
+            int prev2 = 0;
+            int curr = 0;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-
-        return dp[n];
+        return curr;
     }
 };
