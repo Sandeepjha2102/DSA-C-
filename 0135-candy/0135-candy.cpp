b3486713848a58d089bd52ae661f82cffc95abbe
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-19 08:19:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,11 +3,8 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> left(n);
-        vector<int> right(n);
-        int sum = 0;
 
         left[0] = 1;
-        right[n-1] = 1;
 
         //left
         for(int i = 1; i < n; i++){
@@ -20,18 +17,20 @@ public:
         }
 
         //right
+        int curr = 1;
+        int right = 1;
+        int sum = max(1, left[n-1]);
         for(int i = n-2; i >= 0; i--){
             if(ratings[i] > ratings[i+1]){
-                right[i] = right[i+1] + 1;
+                curr = right+1;
+                right = curr;
             }
             else{
-                right[i] = 1;
+                curr = 1;
             }
+            sum = sum + max(left[i], curr);
         }
 
-        for(int i = 0; i < n; i++){
-            sum = sum + max(left[i], right[i]);
-        }
         return sum;
     }
 };
