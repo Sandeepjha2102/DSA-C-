7cd697f002fbc76f790ec1fb2357105775f74181
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-23 03:31:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string updatedS;
+        string updatedString;
         for(int i = 0; i < s.size(); i++){
-            if(s[i] >= 'a' && s[i] <= 'z'){
-                updatedS += s[i];
+            if(s[i] >= 'A' && s[i] <= 'Z'){
+                updatedString += tolower(s[i]);
             }
-            else if(s[i] >= 'A' && s[i] <= 'Z'){
-                    updatedS += tolower(s[i]);
-                }
+            else if(s[i] >= 'a' && s[i] <= 'z'){
+                updatedString += s[i];
+            }
             else if(s[i] >= '0' && s[i] <= '9'){
-                    updatedS += s[i];
-                }    
+                updatedString += s[i];
+            }
             else{
                 continue;
-            }}
-        
-        int i = 0;
-        int j = updatedS.size()-1;
-
-        while (i <= j){
-            if(updatedS[i] == updatedS[j]){
-                i++;
-                j--;
-            }
-            else{
-                return false;
             }
         }
+        int n = updatedString.size();
+        if(n==0) return true;
+        int i = 0;
+        int j = n - 1;
+
+        while(i <= j){
+            if(updatedString[i] != updatedString[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
         return true;
+        
     }
 };
