d3119911819950376e2e91e708d289790f3cfb22
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-24 05:47:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -6,4 +6,4 @@ left join examinations e
 on s.student_id = e.student_id 
 and e.subject_name = sub.subject_name
 group by s.student_id, s.student_name, sub.subject_name
-order by s.student_id, s.student_name;
+order by s.student_id, sub.subject_name;
