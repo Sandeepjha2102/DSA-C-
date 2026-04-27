44c2eb399bc3b53076a05241844459a44f097206
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-27 17:36:16 +0530

    Time: 902 ms (10.45%), Space: 11.5 MB (93.01%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int solve(vector<int>& nums, int i, int curSum, int target){
+        if(i == nums.size()){
+            if(curSum == target) return 1;
+            else return 0;
+        }
+
+        int plus = solve(nums, i+1, curSum+nums[i], target);
+        int minus = solve(nums, i+1, curSum-nums[i], target);
+
+        return plus+minus;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+       return solve(nums, 0, 0, target);
+    }
+};
