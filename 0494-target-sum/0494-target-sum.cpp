6f7e19abcbcf5a9627fa23975e55c8522e81e934
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-27 17:54:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
 
-    int solve(vector<int>& nums, int i, int curSum, int target){
+    int solve(vector<int>& nums, int i, int curSum, int target, int maxSum,vector<vector<int>>& dp){
         if(i == nums.size()){
             if(curSum == target) return 1;
             else return 0;
         }
 
-        int plus = solve(nums, i+1, curSum+nums[i], target);
-        int minus = solve(nums, i+1, curSum-nums[i], target);
+        if(dp[i][curSum+maxSum] != -1) return dp[i][curSum+maxSum];
 
-        return plus+minus;
+        int plus = solve(nums, i+1, curSum+nums[i], target, maxSum, dp);
+        int minus = solve(nums, i+1, curSum-nums[i], target, maxSum, dp);
+
+        return dp[i][curSum+maxSum] = plus+minus;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-       return solve(nums, 0, 0, target);
+        int n = nums.size();
+        int maxSum = 0;
+        for(int i = 0; i < n; i ++){
+            maxSum += nums[i];
+        }
+        vector<vector<int>> dp(n, vector<int>(2*maxSum+1 , -1));
+        return solve(nums, 0, 0, target, maxSum, dp);
     }
 };
