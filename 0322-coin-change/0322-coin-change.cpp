7ce6e7e65d3dd0eccae10a0c8828ae1aaef6ed7b
Sandeep Kumar Jha <144441543+Sandeepjha2102@users.noreply.github.com>
2026-04-27 16:31:35 +0530

    Time: 47 ms (31.14%), Space: 48.8 MB (39.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
 
-    int solve(int ind, vector<int>& coins, int amount, vector<vector<int>>& dp){
-        // if(amount == 0) return MinCoins;
-        if(ind == 0){
-            if(amount % coins[ind] == 0){
-                return amount / coins[ind];
-            }
-            else{
-                return 1e9;
-            }
-        }
-
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-
-        int notTake = 0 + solve(ind-1, coins, amount, dp);
-        int take = INT_MAX;
-        if(amount >= coins[ind]){
-            take = 1 + solve(ind, coins, amount-coins[ind], dp);
-        }
-        return dp[ind][amount] = min(notTake, take);    
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(n-1, coins, amount, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+
+        //base case
+        for(int amt =0; amt <= amount; amt++){
+            if(amt % coins[0] == 0) 
+            dp[0][amt] = amt / coins[0];
+            else dp[0][amt] = 1e9;
+        }
+
+        //nested loop 
+        for(int i = 1; i < n; i++){
+            for(int amt = 0; amt <= amount; amt++){
+                int notTake = dp[i-1][amt];
+                    int take = INT_MAX;
+                if(amt >= coins[i]) take = 1 + dp[i][amt-coins[i]];
+
+            dp[i][amt] = min(take, notTake);
+            }
+        }
+
+        int ans = dp[n-1][amount];
         return (ans >= 1e9 ? -1 : ans);
     }
 };
